51f84410b4d0321c88734cfd05b0e4f3505781c2
Joe Monkila <jnkiwa25@gmail.com>
2025-05-06 11:21:22 +0100

    .

---
+++ src/main/java/com/monkilatech/spring_rate_limiter/config/RateLimiterConfig.java
@@ -0,0 +1,11 @@
+package com.monkilatech.spring_rate_limiter.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class RateLimiterConfig {
+}
+
